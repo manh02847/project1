ed13cfedb50e05698828b423534840e6b49651b3
manh02847 <142043172+manh02847@users.noreply.github.com>
2026-08-26 16:01:34 +0700

    Add files via upload

---
+++ NnmLesson02Review/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NnmLesson02Review.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
