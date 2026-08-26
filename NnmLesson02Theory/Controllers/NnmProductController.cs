c0dfee7bbec24b76ee0740e58bf8a107b1cd894d
manh02847 <142043172+manh02847@users.noreply.github.com>
2026-08-26 16:06:10 +0700

    Add files via upload

---
+++ NnmLesson02Theory/Controllers/NnmProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NnmLesson02Theory.Models;
+
+namespace NnmLesson02Theory.Controllers
+{
+    public class NnmProductController : Controller
+    {
+        public IActionResult NnmIndex()
+        {
+            // Dữ liệu lưu trong các đối tượng: ViewBag, ViewData, TempData
+            ViewBag.name = "Nguyễn Ngọc Mạnh";
+            ViewData["productVD"] = "Laptop Dell Vostro";
+            TempData["UNI"] = "Trường Đại học Nguyễn Trãi - NTU";
+
+            return View();
+        }
+
+        public IActionResult GetProduct()
+        {
+            // Tạo mock data product
+            NnmProduct nnmProduct = new NnmProduct()
+            {
+                ProductID = "2410900051",
+                ProductName = "Nguyễn Ngọc Mạnh",
+                YearRelease = 2006,
+                Price = 1000
+            };
+
+            ViewBag.product = nnmProduct;
+            ViewData["product"] = nnmProduct;
+
+            return View("Product");
+        }
+    }
+}
---
+++ NnmLesson02Theory/Models/NnmProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace NnmLesson02Theory.Models
+{
+    public class NnmProduct
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int YearRelease { get; set; }
+        public decimal Price { get; set; }
+    }
+}
