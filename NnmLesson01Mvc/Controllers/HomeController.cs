55a46bfd14d08eec288a372c72f180292114a481
manh02847 <142043172+manh02847@users.noreply.github.com>
2026-09-06 21:28:23 +0700

    Add NnmLesson01Mvc

---
+++ NnmLesson01Mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using NnmLesson01Mvc.Models;
+
+namespace NnmLesson01Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.name = "Nguyễn Ngọc Mạnh";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
